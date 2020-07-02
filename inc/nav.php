56fce99c15312c29aa3a6081de28f62f3df5d927
Bonnie Yang <bonniehyang@gmail.com>
2020-07-02 12:10:07 -0400

    Updated nav inc and changed naming of pages

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 <nav class="menu">
     <ul>
       <li><a href="index.php">Home</a></li>
-      <li><a href="hyindex.php">Huiyu Yang</a></li>
-      <li><a href="mkindex.php">Molly Kilian</a></li>
-      <li><a href="initalsindex.php">Name</a></li>
+      <li><a href="hy.php">Huiyu Yang</a></li>
+      <li><a href="mk.php">Molly Kilian</a></li>
+      <li><a href="ja.php">Jaafar Hadab</a></li>
     </ul>
  </nav>

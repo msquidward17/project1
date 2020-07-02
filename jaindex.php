add8d782d6a2c281d16f07a75caff0c287617383
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-07-02 18:47:21 +0300

    added webpages and link

---
+++ jaindex.php
@@ -0,0 +1,99 @@
+<?php include "inc/html-top.php"; ?>
+
+<body>
+<?php include "inc/nav.php"; ?>
+<?php include "inc/scripts.php"; ?>
+	<div class = "bigbox">
+		<?php include "inc/nav.php"; ?>
+
+		<header class = "title">
+			<a href="index.php">
+				<h1>Jaafar Hadab</h1>
+			</a>
+		</header>
+
+		<div class="smallbox">
+
+			<div class = "background">
+			</div>
+
+			
+			
+			<p>I am a Mechanical Engineering Student in the University of Rochester. I am experienced in CAD modeling, Finite Element Analysis (FEA), and Manufacturing. I am also minoring in Computer Science and Psychology, from which I got experience in fundemental coding in Java and Python.</p>
+
+		
+			
+		</div>
+	</div>
+
+	<div class = "bigbox">
+		<?php include "inc/nav.php"; ?>
+
+		<header class = "title">
+			<a href="index.php">
+				<h1>Jaafar Hadab</h1>
+			</a>
+		</header>
+
+		<div class="smallbox">
+
+			<div class = "background">
+			</div>
+
+			<h2>Research</h2>
+			
+			<p>I currently work with the Dias laboratory group in the University of Rochester. The Dias group uses Diamond Anvil Cells (DACs) to study matter at extreme states. My current work with the group involves using Finite Element Models to study the shapes of the tips of diamonds in DACs in order to optimize for highest possible pressure.</p>
+
+		
+			
+		</div>
+	</div>
+
+	<div class = "bigbox">
+		<?php include "inc/nav.php"; ?>
+
+		<header class = "title">
+			<a href="index.php">
+				<h1>Jaafar Hadab</h1>
+			</a>
+		</header>
+
+		<div class="smallbox">
+
+			<div class = "background">
+			</div>
+
+			<h2>Baja SAE</h2>
+			
+			<p>I am a member of the University of Rochester Baja SAE team. Each year, we design and build an offroad vehicle to compete with other baja teams from all around the world. I currently serve as the suspension team.</p>
+
+		
+			
+		</div>
+	</div>
+
+	<div class = "bigbox">
+		<?php include "inc/nav.php"; ?>
+
+		<header class = "title">
+			<a href="index.php">
+				<h1>Jaafar Hadab</h1>
+			</a>
+		</header>
+
+		<div class="smallbox">
+
+			<div class = "background">
+			</div>
+
+			<h2>Contact</h2>
+			
+			<p>If you'd like help or more information, I'd be more than happy to provide. I can be easily reached out to at email@gmail.com or if on phone at 555-555-5555.</p>
+
+		
+			
+		</div>
+	</div>
+</body>
+
+</html>
--- page4.php
+++
@@ -1,8 +0,0 @@
-<?php include "inc/html-top.php"; ?>
-
-<body>
-<?php include "inc/nav.php"; ?>
-<?php include "inc/scripts.php"; ?>
-</body>
-
-</html>

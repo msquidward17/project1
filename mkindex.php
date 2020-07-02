c1e3aed69c6093220314f734569641a35a66f3c4
msquidward17 <67696489+msquidward17@users.noreply.github.com>
2020-07-02 09:43:10 -0400

    Add files via upload

---
+++ mkindex.php
@@ -0,0 +1,168 @@
+<?php include "inc/html-top.php"; ?>
+<body>
+	<div class="container">
+	<header>
+		<a href="index.php">
+		<h1>Molly Kilian</h1>
+		<figure><img src="images/headshot.png" alt="Molly Kilian"></figure>
+		<div class="div">Student at the University of Rochester</div>
+		</a>
+	</header>
+	
+	<?php include "inc/nav.php"; ?>
+
+	<article>
+		<p>Molly Kilian is an active student at the University of Rochester. She pushes herself academically and creatively, and believes in treating others with kindness.</p>
+		<p>She is from Utica, NY, and has one older brother and two cats. Her mother is an art teacher and her father is a newspaper editor.</p>
+	</article>
+	
+	<section>
+	<p>Molly Kilian is a rising sophomore at the University of Rochester, interested in a wide variety of subjects. She is planning to major in some form of humanities or social science, and to minor in Computer Science and/or Digital Media Studies.</p>
+		<p>She has taken the following classes in her time at U of R:</p>
+		<h2>Fall 2019</h2>
+			<ul>
+				<li>GSW 100 - Working Women: Labor Activism</li>
+				<li>MTH 171 - Honors Calculus 1</li>
+				<li>MUR 104 - Carillon</li>
+				<li>CSC 161 - Introduction to Programming</li>
+				<li>HIS 223 - European Nationalisms</li>
+			</ul>
+		<h2>Spring 2020</h2>
+			<ul>
+				<li>HIS 252W - Immigration and the Americas</li>
+				<li>MTH 172 - Honors Calculus 2</li>
+				<li>MUR 104 - Carillon</li>
+				<li>CSC 170 - Introduction to Web Development</li>
+				<li>CSC 171 - Introduction to Computer Science</li>
+				<li>EHU 284 - Civil Disobedience</li>
+				</ul>
+		<h2>Summer 2020</h2>
+			<ul>
+				<li>MTH 165 - Linear Algebra with Differential Equations</li>
+				<li>DMS 101 - Introduction to Digital Media Studies</li>
+				<li>CSC 174 - Advanced Front End Web Development</li>
+			</ul>
+	</section>
+	
+	<section>
+	<div class="grida">
+	<div>
+	<article>
+	<p>At the University of Rochester, Molly is an active member of Creative Arts Club, Vice President of Cinema Group, and plays the carillon.<p>
+	<p>Off campus, Molly is a collegiate member of Clinton Figure Skating Club and an associate member of Genesee Figure Skating Club. She is working on her juvenile moves in the field and her preliminary freeskate. Her favorite figure skater is Evgenia Medvedeva.</p>
+	<p>Molly is an active volunteer at her church, and a volunteer at a local animal shelter (Spring Farms Cares).</p>
+	<p>She enjoys spending her time in book clubs, learning new things, making art, playing music, and baking.</p>
+	</article>
+	</div>
+	
+	<div>
+	<aside>
+	<h2>Other activities:</h2>
+		<ul>
+			<li>Drawing and painting classes</li>
+			<li>Interning at Fiveable (currently a Founding Member)</li>
+			<li>Piano, viola, and violin lessons</li>
+			<li>Photography</li>
+			<li>Mixed media art</li>
+			<li>Roller skating</li>
+			<li>Travelling</li>
+			<li>Attending musicals</li>
+			<li>Watching Disney+</li>
+		</ul>
+	</aside>
+	</div>
+	</div>
+	
+	</section>
+	
+	<section class="interests">
+		<p>Molly has a wide variety of interests, including figure skating, baking, film, reading, music, thrifting, and art.</p>
+		<h2>Favorite things:</h2>
+		<div class="gridi">
+		<div>
+		<h3>Movies</h3>
+			<ul>
+				<li>Dead Poets Society</li>
+				<li>The Spongebob Squarepants Movie</li>
+				<li>Christopher Robin</li>
+			</ul>
+		</div>
+		<div>
+		<h3>Books</h3>
+			<ul>
+				<li>The Astonishing Color of After</li>
+				<li>Ashes in the Snow</li>
+				<li>What the Night Sings</li>
+			</ul>
+		</div>
+		<div>
+		<h3>Musicals</h3>
+			<ul>
+				<li>Hamilton</li>
+				<li>Dear Evan Hansen</li>
+				<li>The Spongebob Squarepants Musical</li>
+			</ul>
+		</div>
+		<div>
+		<h3>Musicians</h3>
+			<ul>
+				<li>Ben Platt</li>
+				<li>Vince Guaraldi</li>
+				<li>Chopin</li>
+			</ul>
+		</div>
+		<div>
+		<h3>TV Shows</h3>
+			<ul>
+				<li>Andi Mack</li>
+				<li>Spongebob Squarepants</li>
+				<li>Jeopardy</li>
+			</ul>
+		</div>
+		<div>
+		<h3>Figure Skaters</h3>
+			<ul>
+				<li>Evgenia Medvedeva</li>
+				<li>Alena Kostornaia</li>
+				<li>Gracie Gold</li>
+			</ul>
+		</div>
+		<div>
+		<h3>Animals</h3>
+			<ul>
+				<li>Rabbits</li>
+				<li>Cats</li>
+				<li>Otters</li>
+			</ul>
+		</div>
+		<div>
+		<h3>Seasons</h3>
+			<ol>
+				<li>Winter</li>
+				<li>Fall</li>
+				<li>Spring</li>
+			</ol>
+		</div>
+		<div>
+		<h3>Colors</h3>
+			<ul>	
+				<li>Yellow</li>
+				<li>Purple</li>
+				<li>Peach</li>
+			</ul>
+		</div>
+		
+		</div>
+	</section>
+	
+	<aside>
+		<p id="aamilne">"You're braver than you believe, stronger than you seem, and smarter than you think."<span> ~ Milne</span></p>
+		<p id="play">"The powerful play goes on, and you may contribute a verse."<span> ~ Whitman</span></p>
+		<p id="tolstoy">"There is something in the human spirit that will survive and prevail, there is a tiny and brilliant light burning in the heart of man that will not go out no matter how dark the world becomes."<span> ~ Tolstoy</span></p>
+		<p id="lived">"...and not, when I had come to die, discover that I had not lived."<span> ~ Thoreau</span></p>
+	</aside>
+	
+</div>
+<?php include "inc/scripts.php"; ?>
+</body>
+</html>

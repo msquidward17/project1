34ba60843d09c42729948f846879e8e63c2f059a
msquidward17 <67696489+msquidward17@users.noreply.github.com>
2020-07-10 13:41:19 -0400

    Started making classes and triples, organizing content, and changing to third person POV

--- jp.php
+++ jp.php
@@ -10,29 +10,51 @@
 		</header>
 
 		<div class="article">
-			<p>My name is Joo Eon Park. I am from South Korea, and was born and raised there my entire life. I'm fluent in both English and Korean (and speak a bit of French)!</p>
-			<p>I'm currently a student at the University of Rochester. Read more about some of the things I like below!</p>
+			<p>Joo Eon Park (pronouns) is a rising _____ at the University of Rochester. He is studying ____.</p>
+			<p>He is from South Korea, and was born and raised there. He is fluent in both English and Korean (and speaks a bit of French).</p>
 		</div>
 		</div>
 	</div>
 <div class="container">
 		<section>
-			<h2>Art</h2>
-			<p>I've always had great interest in art and design. One of my favorite things to do is go to museums and galleries. To keep my interest in art alive, I took a course on printmaking last semester. This was the first time I did printmaking, and it was a lot of fun. This piece was inspired by M.C. Escher's work. This type of pattern is called a tessellation. Linoleum blocks were carved, inked, and pressed onto the paper. This technique is called relief printing.</p>
-			<figure><img src="images/jp_art.jpg" alt="Joo Eon Park"></figure>
+			<h2>Academics</h2>
+			<p>Joo Eon Park is a student at the University of Rochester, majoring in -_______add minors and clusters if applicable.___-
+			<p>Here are some of the courses he has taken in his time at the University of Rochesr:</p>
+			<ul>
+				<li>-insert, see Molly or Michael's page for inspiration</li>
+			</ul>
+		</section>
+		
+		<section>
+			<h2>Interests</h2>
+				<h3>The Arts</h3>
+					<h4>Art</h4>
+						<p>I've always had great interest in art and design. One of my favorite things to do is go to museums and galleries. To keep my interest in art alive, I took a course on printmaking last semester. This was the first time I did printmaking, and it was a lot of fun. This piece was inspired by M.C. Escher's work. This type of pattern is called a tessellation. Linoleum blocks were carved, inked, and pressed onto the paper. This technique is called relief printing.</p>
+						<figure><img src="images/jp_art.jpg" alt="Joo Eon Park"></figure>
+				<h3>Music</h3>
+					<p>I've always been in close proximity to music, mainly due to my dad's career in the musical world. I most closely relate to rock music and the electric guitar, having listened to various rock genres and bands since I was young. I most fondly recall listening to Jimi Hendrix in my dad's car. At the time, I had no regard for what I was listening to, but as I grew up I developed a great appreciation for Hendrix and music in general.</p>
+					<p>Now, I'm always with a guitar, practicing on the daily. I play in the Rock Ensemble at school, take Jazz guitar lessons at the Eastman School of Music, and have begun recording and mixing my guitar playing. I hope to begin writing my own songs one day.</p>
+					<figure><img src="images/jp_music.jpg" alt="Joo Eon Park"></figure>
 		</section>
 
 		<section>
-			<h2>Music</h2>
-			<p>I've always been in close proximity to music, mainly due to my dad's career in the musical world. I most closely relate to rock music and the electric guitar, having listened to various rock genres and bands since I was young. I most fondly recall listening to Jimi Hendrix in my dad's car. At the time, I had no regard for what I was listening to, but as I grew up I developed a great appreciation for Hendrix and music in general.</p>
-			<p>Now, I'm always with a guitar, practicing on the daily. I play in the Rock Ensemble at school, take Jazz guitar lessons at the Eastman School of Music, and have begun recording and mixing my guitar playing. I hope to begin writing my own songs one day.</p>
-			<figure><img src="images/jp_music.jpg" alt="Joo Eon Park"></figure>
+			<h2>Activities</h2>
+				<h3>On campus</h3>
+				<p>rock ensemble, eastman lessons</p>
 		</section>
 
 		<section>
-			<h2>My Cat</h2>
-			<p>My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless.</p>
-			<figure><img src="images/jp_cat.png" alt="Joo Eon Park"></figure>
+			<h2>Fun facts</h2>
+				<h3>Skills</h3>
+					<p>Joo Eon speaks a variety of languages. He has good computer programming skills, great aristic skills, and is good at music.</p>
+				<h3>Pets</h3>
+					<p>Joo Eon has a cat. Hear what () has to say:</p>
+					<p>"My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless."</p>
+					<figure><img src="images/jp_cat.png" alt="Joo Eon Park"></figure>
+				<h3>Favorite things</h3>
+					<ul>
+						<li></li>
+					</ul>
 		</section>
 
 		<aside>

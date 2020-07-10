8e978454398d77da8e0c23973fffc2a8e23846a5
Joo Eon Park <jpark174@u.rochester.edu>
2020-07-10 22:22:48 +0900

    Added images and php content
    - added four images
    - added content to jp.php

--- jp.php
+++ jp.php
@@ -5,26 +5,38 @@
 	<div class="blacktop">
 		<div class = "blackcontent">
 		<header>
-			<figure><img src="images/" alt="Joo Eon Park"></figure>
+			<figure><img src="images/jp_self" alt="Joo Eon Park"></figure>
 			<h1>Joo Eon Park</h1>
 		</header>
 
 		<div class="article">
+			<p>My name is Joo Eon Park. I am from South Korea, and was born and raised there my entire life. I'm fluent in both English and Korean (and speak a bit of French)!</p>
+			<p>I'm currently a student at the Universiyt of Rochester. Read more about some of the things I like below!</p>
 		</div>
 		</div>
 	</div>
 <div class="container">
 		<section>
+			<h2>Art</h2>
+			<p>I've always had great interest in art and design. One of my favorite things to do is go to museums and galleries. To keep my interest in art alive, I took a course on printmaking last semester. This was the first time I did printmaking, and it was a lot of fun. This piece was inspired by M.C. Escher's work. This type of pattern is called a tessellation. Linoleum blocks were carved, inked, and pressed onto the paper. This technique is called relief printing.</p>
+			<figure><img src="images/jp_art" alt="Joo Eon Park"></figure>
 		</section>
 
 		<section>
+			<h2>Music</h2>
+			<p>I've always been in close proximity to music, mainly due to my dad's career in the musical world. I most closely relate to rock music and the electric guitar, having listened to various rock genres and bands since I was young. I most fondly recall listening to Jimi Hendrix in my dad's car. At the time, I had no regard for what I was listening to, but as I grew up I developed a great appreciation for Hendrix and music in general.</p>
+			<p>Now, I'm always with a guitar, practicing on the daily. I play in the Rock Ensemble at school, take Jazz guitar lessons at the Eastman School of Music, and have begun recording and mixing my guitar playing. I hope to begin writing my own songs one day.</p>
+			<figure><img src="images/jp_music" alt="Joo Eon Park"></figure>
 		</section>
 
 		<section>
+			<h2>My Cat</h2>
+			<p>My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless.</p>
+			<figure><img src="images/jp_cat" alt="Joo Eon Park"></figure>
 		</section>
 
 		<aside>
-			<p id="quote2">"Quote"<span>- name</span></p>
+			<p id="quote2">"The story of life is quicker than the wink of an eye, the story of love is hello and goodbye...until we meet again."<span> ~ Jimi Hendrix</span></p>
 		</aside>
 
 		<footer>

0f98404ed89ab01fa73579be4e18c5be3e056bb0
Michael B <mbashner@u.rochester.edu>
2020-07-10 11:03:26 -0400

    Update html-top.php
    Slightly changed the formatting to make editing easier

--- inc/html-top.php
+++ inc/html-top.php
@@ -1,10 +1,9 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Project 2</title>
-	<link rel="stylesheet" href="css/styles.css">
-	<link rel="stylesheet" href="css/navigation.css">
-	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Niconne&display=swap" rel="stylesheet">
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>Project 2</title>
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/navigation.css">
+		<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Niconne&display=swap" rel="stylesheet">
+	</head>

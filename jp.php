1e8b0c97a6eecff4d4c2cc446e85baa3de9c2b77
Joo Eon Park <jpark174@u.rochester.edu>
2020-07-11 18:56:17 +0900

    Added more images and content

--- jp.php
+++ jp.php
@@ -10,7 +10,7 @@
 		</header>
 
 		<div class="article">
-			<p>Joo Eon Park (pronouns) is a rising _____ at the University of Rochester. He is studying ____.</p>
+			<p>Joo Eon Park (he/him) is a rising junior at the University of Rochester. He has a wide range of interests, including art, music, and technology, and tries his best to learn from a variety of different experiences available to him in college.</p>
 			<p>He is from South Korea, and was born and raised there. He is fluent in both English and Korean (and speaks a bit of French).</p>
 		</div>
 		</div>
@@ -18,11 +18,32 @@
 <div class="container">
 		<section>
 			<h2>Academics</h2>
-			<p>Joo Eon Park is a student at the University of Rochester, majoring in -_______add minors and clusters if applicable.___-
+			<p>Joo Eon Park is a student at the University of Rochester, majoring in Computer Science with a minor in Digital Media Studies. His intended clusters are Music and Public Health.</p>
 			<p>Here are some of the courses he has taken in his time at the University of Rochester:</p>
-			<ul>
-				<li>-insert, see Molly or Michael's page for inspiration</li>
-			</ul>
+			<h3>Computer Science</h3>
+				<ul>
+					<li>CSC 170 - Introduction to Web Development</li>
+					<li>CSC 171 - Introduction to Computer Science</li>
+					<li>CSC 172 - Data Structures & Algorithms</li>
+					<li>CSC 173 - Computation & Formal Systems</li>
+					<li>CSC 186 - Video Game Development</li>
+				</ul>
+			<h3>Music</h3>
+				<ul>
+					<li>MUR 101 - Elements of Music</li>
+					<li>MUR 109 - Musicianship</li>
+					<li>MUR 180 - Rock Repertory Ensemmble</li>
+					<li>JAZ 130 - Jazz Guitar Lessons at Eastman</li>
+					<li></li>
+				</ul>
+			<h3>Other Interesting Courses</h3>
+				<ul>
+					<li>AME 140 - Introduction to Audio & Music Engineering</li>
+					<li>DMS 103 - Essential Digital Media Toolkit</li>
+					<li>DMS 110 - Video Game History</li>
+					<li>DMS 120 - Video Game Design</li>
+					<li>SA 181 - Introduction to Printmaking</li>
+				</ul>
 		</section>
 		
 		<section>
@@ -30,13 +51,13 @@
 				<h3>The Arts</h3>
 					<h4>Art</h4>
 						<p>Joo Eon has always had great interest in art and design. One of his favorite things to do is go to museums and galleries. To keep his interest in art alive, he took a course on printmaking last semester. This was the first time he did printmaking, and it was a lot of fun. This piece was inspired by M.C. Escher's work. This type of pattern is called a tessellation. Linoleum blocks were carved, inked, and pressed onto the paper. This technique is called relief printing.</p>
-						<figure><img src="images/jp_art.jpg" alt="Joo Eon Park"><figcaption>caption</figcaption></figure>
-						<figure><img src="images/" alt=""><figcaption>caption</figcaption></figure>
+						<figure><img src="images/jp_art.jpg" alt="jooeon_art1"><figcaption><em>Symbiosis</em>, Relief Print</figcaption></figure>
+						<figure><img src="images/jp_art2.jpg" alt="jooeon_art2"><figcaption>Cross-hatching Sketch</figcaption></figure>
 				<h3>Music</h3>
 					<p>Joo Eon has always been in close proximity to music, mainly due to his dad's career in the musical world. He most closely relates to rock music and the electric guitar, having listened to various rock genres and bands since he was young. He most fondly recalls listening to Jimi Hendrix in his dad's car. At the time, he had no regard for what he was listening to, but as he grew up, he developed a great appreciation for Hendrix and music in general.</p>
 					<p>Now, he is always with a guitar, practicing on the daily. He plays in the Rock Ensemble at school, take Jazz guitar lessons at the Eastman School of Music, and have begun recording and mixing my guitar playing. He hopes to begin writing my own songs one day.</p>
-					<figure><img src="images/jp_music.jpg" alt="Joo Eon Park"><figcaption>caption</figcaption></figure>
-					<figure><img src="images/" alt=""><figcaption>caption</figcaption></figure>
+					<figure><img src="images/jp_music.jpg" alt="guitar_in_studio"><figcaption>Playing guitar in studio</figcaption></figure>
+					<figure><img src="images/jp_music2.jpg" alt="guitar_on_stage"><figcaption>Playing guitar on stage</figcaption></figure>
 		</section>
 
 		<section>
@@ -45,8 +66,8 @@
 				<div>
 					<div class="article">
 					<h3>On campus</h3>
-						<p>At the University of Rochester, Joo Eon is an active member of _________<p>
-						<p>When not in class, he can be found _________</p>
+						<p>At the University of Rochester, Joo Eon is an active member of Cinema Group, Meliora Fitness, and plays guitar for the Rock Repertory Ensemble.<p>
+						<p>When not in class, he can be found playing video games, studying, playing the guitar, listening to music, or browsing the internet.</p>
 					</div>
 				</div>
 
@@ -54,7 +75,13 @@
 				<aside>
 				<h3>Other activities:</h3>
 					<ul>
-						<li>list</li>
+						<li>Doing art</li>
+						<li>Viewing art - museums/galleries</li>
+						<li>Exercising</li>
+						<li>Biking</li>
+						<li>Playing Ultimate Frisbee</li>
+						<li>Watching movies</li>
+						<li>Reading novels or comic books</li>
 					</ul>
 				</aside>
 				</div>
@@ -68,71 +95,89 @@
 				<h3>Pets</h3>
 					<p>Joo Eon has a cat. Hear all about it:</p>
 					<p>"My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless."</p>
-					<figure><img src="images/jp_cat.png" alt="Joo Eon Park"><figcaption>caption</figcaption></figure>
+					<figure><img src="images/jp_cat.png" alt="jooeons_cat"><figcaption>Haku, 9 years old.</figcaption></figure>
 				<h3>Favorite things</h3>
 				<div class="gridi">
 					<div>
-						<h4>item</h4>
+						<h4>Musicians</h4>
 							<ul>
-								<li>list</li>
+								<li>Jimi Hendrix</li>
+								<li>John Frusciante</li>
+								<li>Kurt Cobain</li>
+							</ul>
+					</div>
+
+					<div>
+						<h4>Bands</h4>
+							<ul>
+								<li>Hyukoh</li>
+								<li>Led Zeppelin</li>
+								<li>Nirvana</li>
 							</ul>
 					</div>
 					
 					<div>
-						<h4>item</h4>
+						<h4>Artists</h4>
 							<ul>
-								<li>list</li>
+								<li>M.C. Escher</li>
+								<li>Bernard Buffet</li>
+								<li>Banksy</li>
+							</ul>
+					</div>
+
+					<div>
+						<h4>Cuisine</h4>
+							<ul>
+								<li>Indian Food</li>
+								<li>Italian Food</li>
+								<li>Korean Food</li>
 							</ul>
 					</div>
 					
 					<div>
-						<h4>item</h4>
+						<h4>Documentaries</h4>
 							<ul>
-								<li>list</li>
+								<li>Planet Earth</li>
+								<li>Don't F**k With Cats</li>
+								<li>Conversations with a Killer: The Ted Bundy Tapes</li>
 							</ul>
 					</div>
 					
 					<div>
-						<h4>item</h4>
+						<h4>Video Games</h4>
 							<ul>
-								<li>list</li>
+								<li>Mario Kart</li>
+								<li>League of Legends</li>
+								<li>Minecraft</li>
 							</ul>
 					</div>
 					
 					<div>
-						<h4>item</h4>
+						<h4>Colors</h4>
 							<ul>
-								<li>list</li>
+								<li>Black</li>
+								<li>Green</li>
+								<li>Blue</li>
 							</ul>
 					</div>
 					
 					<div>
-						<h4>item</h4>
+						<h4>Countries to visit</h4>
 							<ul>
-								<li>list</li>
+								<li>France</li>
+								<li>Japan</li>
+								<li>Greece</li>
 							</ul>
 					</div>
 					
 					<div>
-						<h4>item</h4>
+						<h4>Snacks</h4>
 							<ul>
-								<li>list</li>
+								<li>Cheetos</li>
+								<li>Oreos</li>
+								<li>Doritos</li>
 							</ul>
 					</div>
-					
-					<div>
-						<h4>item</h4>
-							<ul>
-								<li>list</li>
-							</ul>
-					</div>
-					
-					<div>
-						<h4>item</h4>
-							<ul>
-								<li>list</li>
-							</ul>
-					</div>	
 				</div>
 		</section>
 

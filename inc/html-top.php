4b9297054f8526834cf605d7993bb6ef99d28bd2
Michael B <mbashner@u.rochester.edu>
2020-07-12 22:49:38 -0400

    Meta Description and updated script
    Added the meta description and updated the script php include to use the most recent version of the jquery cdn

--- inc/html-top.php
+++ inc/html-top.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Project 2 London Team">
 		<title>Project 2</title>
 		<link rel="stylesheet" href="css/styles.css">
 		<link rel="stylesheet" href="css/navigation.css">

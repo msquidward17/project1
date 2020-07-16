b2b9467c4040b8a8755350305acb3470b774f336
Adira Blumenthal <adiraeb@gmail.com>
2020-07-16 17:52:38 -0400

    fixed jp.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include "inc/html-top.php"; ?>
+<div class="container">
+
+<p>This will be the Z-pattern design</p>
+<a href="secondary.php" class="rdmore">About Our Team</a>
+
+<footer>
+	<p>
+	CSC 174: Advanced Front-end Web Design and Development -
+	<a href="http://docs.csc174.org" target=_blank>Course home page</a>
+	</p>
+</footer>
+
+</div>
+<?php include "inc/scripts.php"; ?>
+</body>
+</html>
--- jp.php
+++ jp.php
@@ -12,10 +12,15 @@
 					</div>
 				</header>
 				<section class="container">
-					<h3>Academics</h3>
+					<h2>Skills</h2>
+					<p>Joo Eon speaks a variety of languages. He has good computer programming skills, great artistic skills, and is good at music.</p>
+				</section>
+
+				<section class="container">
+					<h2>Academics</h2>
 					<p>Joo Eon Park is a student at the University of Rochester, majoring in Computer Science with a minor in Digital Media Studies. His intended clusters are Music and Public Health.</p>
 					<p>Here are some of the courses he has taken in his time at the University of Rochester:</p>
-					<h4>Computer Science</h4>
+					<h3>Computer Science</h3>
 					<ul>
 						<li>CSC 170 - Introduction to Web Development</li>
 						<li>CSC 171 - Introduction to Computer Science</li>
@@ -23,14 +28,14 @@
 						<li>CSC 173 - Computation & Formal Systems</li>
 						<li>CSC 186 - Video Game Development</li>
 					</ul>
-					<h4>Music</h4>
+					<h3>Music</h3>
 					<ul>
 						<li>MUR 101 - Elements of Music</li>
 						<li>MUR 109 - Musicianship</li>
 						<li>MUR 180 - Rock Repertory Ensemmble</li>
 						<li>JAZ 130 - Jazz Guitar Lessons at Eastman</li>
 					</ul>
-					<h4>Other Interesting Courses</h4>
+					<h3>Other Interesting Courses</h3>
 					<ul>
 						<li>AME 140 - Introduction to Audio & Music Engineering</li>
 						<li>DMS 103 - Essential Digital Media Toolkit</li>
@@ -40,11 +45,9 @@
 					</ul>
 				</section>
 				<section class="container">
-					<h3>Interests</h3>
+					<h2>Interests</h2>
 					<section>
-						<h4>The Arts</h4>
 						<section>
-							<h5>Art</h5>
 							<p>Joo Eon has always had great interest in art and design. One of his favorite things to do is go to museums and galleries. To keep his interest in art alive, he took a course on printmaking last semester. This was the first time he did printmaking, and it was a lot of fun. This piece was inspired by M.C. Escher's work. This type of pattern is called a tessellation. Linoleum blocks were carved, inked, and pressed onto the paper. This technique is called relief printing.</p>
 							<figure class="side-by-side">
 								<figure>
@@ -58,35 +61,35 @@
 							</figure>
 						</section>
 						<section>
-							<h5>Music</h5>
+							<h3>Music</h3>
 							<p>Joo Eon has always been in close proximity to music, mainly due to his dad's career in the musical world. He most closely relates to rock music and the electric guitar, having listened to various rock genres and bands since he was young. He most fondly recalls listening to Jimi Hendrix in his dad's car. At the time, he had no regard for what he was listening to, but as he grew up, he developed a great appreciation for Hendrix and music in general.</p>
 							<p>Now, he is always with a guitar, practicing on the daily. He plays in the Rock Ensemble at school, take Jazz guitar lessons at the Eastman School of Music, and has begun recording and mixing his guitar playing. He hopes to begin writing his own songs one day.</p>
 							<figure class="side-by-side">
 								<figure>
 									<img src="images/jp_music.jpg" alt="guitar_in_studio">
-									<figcaption>Playing guitar in studio</figcaption>
+				
 								</figure>
 								<figure>
 									<img src="images/jp_music2.jpg" alt="guitar_on_stage">
-									<figcaption>Playing guitar on stage</figcaption>
+						
 								</figure>
 							</figure>
 						</section>
 					</section>
 				</section>
 				<section class="container">
-					<h3>Activities</h3>
+					<h2>Activities</h2>
 					<div class="grida">
 						<div>
 							<div class="article">
-								<h4>On campus</h4>
+								<h3>On campus</h3>
 								<p>At the University of Rochester, Joo Eon is an active member of Cinema Group, Meliora Fitness, and plays guitar for the Rock Repertory Ensemble.</p>
 								<p>When not in class, he can be found playing video games, studying, playing the guitar, listening to music, or browsing the internet.</p>
 							</div>
 						</div>
 						<div>
 							<aside>
-								<h4>Other activities:</h4>
+								<h3>Other activities:</h3>
 								<ul>
 									<li>Doing art</li>
 									<li>Viewing art - museums/galleries</li>
@@ -101,11 +104,11 @@
 					</div>
 				</section>
 				<section class="container">
-					<h3>Fun facts</h3>
-					<h4>Favorite things</h4>
+					<h2>Fun facts</h2>
+					<h3>Favorite things</h3>
 					<div class="gridi">
 						<div>
-							<h5>Artists</h5>
+							<h4>Artists</h4>
 							<ul>
 								<li>M.C. Escher</li>
 								<li>Bernard Buffet</li>
@@ -113,7 +116,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Bands</h5>
+							<h4>Bands</h4>
 							<ul>
 								<li>Hyukoh</li>
 								<li>Led Zeppelin</li>
@@ -121,7 +124,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Colors</h5>
+							<h4>Colors</h4>
 							<ul>
 								<li>Black</li>
 								<li>Green</li>
@@ -129,7 +132,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Countries to visit</h5>
+							<h4>Countries to visit</h4>
 							<ul>
 								<li>France</li>
 								<li>Japan</li>
@@ -137,7 +140,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Cuisine</h5>
+							<h4>Cuisine</h4>
 							<ul>
 								<li>Indian Food</li>
 								<li>Italian Food</li>
@@ -145,7 +148,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Documentaries</h5>
+							<h4>Documentaries</h4>
 							<ul>
 								<li>Planet Earth</li>
 								<li>Don't F**k With Cats</li>
@@ -153,7 +156,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Musicians</h5>
+							<h4>Musicians</h4>
 							<ul>
 								<li>Jimi Hendrix</li>
 								<li>John Frusciante</li>
@@ -161,7 +164,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Snacks</h5>
+							<h4>Snacks</h4>
 							<ul>
 								<li>Cheetos</li>
 								<li>Oreos</li>
@@ -169,7 +172,7 @@
 							</ul>
 						</div>
 						<div>
-							<h5>Video Games</h5>
+							<h4>Video Games</h4>
 							<ul>
 								<li>Mario Kart</li>
 								<li>League of Legends</li>
@@ -178,22 +181,21 @@
 						</div>
 					</div>
 					<section>
-						<h4>Pets</h4>
+						<h2>Pets</h2>
+						<div class="pets">
+						<div class="one">
 						<p>Joo Eon has a cat. Hear all about it:</p>
-						<p>"My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless."</p>
-					<figure><img src="images/jp_cat.jpg" alt="jooeons_cat"><figcaption>Haku, 9 years old.</figcaption></figure>
+						<p>"My cat's name is Haku. She is named after the character from the film, <em>Spirited Away</em>. Story: We named our cat after Haku from the film because we thought he had blue eyes, just like our cat. Turns out he doesn't... She is a 9 year old Siamese cat, though she will be turning 10 on July 15. She's needy and is sometimes an ass (as cats typically are), but she is quite cute nonetheless."</p></div>
+					<figure><img src="images/jp_cat.jpg" alt="jooeons_cat"><figcaption>Haku, 9 years old.</figcaption></figure></div>
 				</section>
-				<section>
-					<h4>Skills</h4>
-					<p>Joo Eon speaks a variety of languages. He has good computer programming skills, great artistic skills, and is good at music.</p>
-				</section>
+				
 			</section>
 			<aside class="container">
 				<p id="quote2"><q>The story of life is quicker than the wink of an eye, the story of love is hello and goodbye...until we meet again.</q><span> ~ Jimi Hendrix</span></p>
 			</aside>
 		</article>
 	</main>
-	<footer>
+	<footer class="container">
 		<p>
 			CSC 174: Advanced Front-end Web Design and Development -
 			<a href="http://csc174.org/lab01/jpark174/" target=_blank>Original Lab 01</a>

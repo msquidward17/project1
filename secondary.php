fd8faee95801244dac3374c51adfc9765842ba3e
bowu9458 <67699284+bowu9458@users.noreply.github.com>
2020-07-16 17:44:19 -0400

    update the f-pattern

--- secondary.php
+++ secondary.php
@@ -3,32 +3,56 @@
 	
 <article>
 	<h1 class="London">London</h1>
-	
-        <h2>Michael Bashner</h2>
-        <div class="grid">
-	        <figure class="intrf"><img src="images/mb.jpg" alt="Michael Bashner"></figure>
-	        <p class="intrp">Michael Bashner is a student at the University of Rochester majoring in Computer Science, minoring in Linguistics, and clustering in Gender, Sexuality, and Women's Studies.</p>
-	        <a href="mb.php" class="rdmore">Read more</a>
-    	</div>
 
+		<div class="grid">
+			<div class="intrf">
+	        	<h2>Michael Bashner</h2>
+		        <figure ><img src="images/mb.jpg" alt="Michael Bashner"></figure>
+	        </div>
 
+	        <div class="intrp">
+	        	<p>Michael Bashner is a student at the University of Rochester majoring in Computer Science, minoring in Linguistics, and clustering in Gender, Sexuality, and Women's Studies.</p>
+	    	</div>
 
-    	
-        <h2>Molly Kilian</h2>
-        <div class="grid">
-        	<figure class="intrf"><img src="images/headshot.jpg" alt="Molly Kilian"></figure>
-	        <p class="intrp">Molly Kilian is a rising sophomore at the University of Rochester, interested in a wide variety of subjects. She is planning to major in some form of humanities or social science, and to minor in Computer Science and/or Digital Media Studies.</p>
-	      	<a href="mk.php" class="rdmore">Read more</a>
+	    	<div class="rdmore">
+	        	<a href="mb.php" >Read more</a>
+	        </div>
 	    </div>
+
+
+    	<div class="grid">
+    		<div class="intrf">
+	        	<h2>Molly Kilian</h2>
+	        	<figure>
+	        		<img src="images/headshot.jpg" alt="Molly Kilian">
+	        	</figure>
+	        </div>
+
+	        <div class="intrp">
+		        <p>Molly Kilian is a rising sophomore at the University of Rochester, interested in a wide variety of subjects. She is planning to major in some form of humanities or social science, and to minor in Computer Science and/or Digital Media Studies.</p>
+		    </div>
+
+		    <div class="rdmore">
+		      	<a href="mk.php" class="rdmore">Read more</a>
+		    </div>
+		</div>
  
 
+ 		<div class="grid">
+    		<div class="third-row">
+	        	<h2>Joo Eon Park</h2>
+		        <figure>
+		        	<img src="images/jp_self.jpg" alt="Joo Eon Park">
+		        </figure>
+		    </div>
 
-    	
-        <h2>Joo Eon Park</h2>
-        <div class="grid">
-	        <figure class="intrf"><img src="images/jp_self.jpg" alt="Joo Eon Park"></figure>
-	        <p class="intrp">Joo Eon Park is a rising junior at the University of Rochester, majoring in Computer Science and minoring in Digital Media Studies. </p>
-	        <a href="jp.php" class="rdmore">Read more</a>
+		    <div class="intrp">
+		        <p>Joo Eon Park is a rising junior at the University of Rochester, majoring in Computer Science and minoring in Digital Media Studies. </p>
+		    </div>
+
+		    <div class="rdmore">
+		        <a href="jp.php" class="rdmore">Read more</a>
+		    </div>
 	    </div>
    
  </article>

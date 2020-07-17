4c5b22f91675c4655249ddfac19712aa7c4c45d3
bowu9458 <67699284+bowu9458@users.noreply.github.com>
2020-07-17 17:09:54 -0400

    Home page (z-pattern)

---
+++ inc/home-html-top.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>CSC 174 - Project3</title>
+	<link rel="stylesheet" type="text/css" href="css/home.css">
--- index.php
+++ index.php
@@ -1,17 +1,56 @@
-<?php include "inc/html-top.php"; ?>
-<div class="container">
+<?php include "inc/home-html-top.php" ?>
 
-<p>This will be the Z-pattern design</p>
-<a href="secondary.php" class="rdmore">About Our Team</a>
+</head>
+<body>
+	<section class="upper-left">
+		<img src="images/logo.png" alt="csc 174">
+		<div class="number-semester">
+			<div class="semester">2020 Summer</div>
+			<div class="class-number">CSC 174</div>
+		</div>
+	</section>
 
-<footer>
-	<p>
-	CSC 174: Advanced Front-end Web Design and Development -
-	<a href="http://docs.csc174.org" target=_blank>Course home page</a>
-	</p>
-</footer>
 
-</div>
-<?php include "inc/scripts.php"; ?>
+	<section class="upper-right">
+		<div class="login-container">
+			<a href="">Login</a>
+		</div>
+	</section>
+
+
+	<section class="middle">
+		<div class="welcome">Welcome!</div>		
+	</section>
+
+
+
+	<section class="bottom-left">
+		<div class="name-reference">
+			<div class="class-name">Advanced Front-end Web Design and Development</div>
+			<img src="images/book-reference.png"  alt="book-logo">
+			<a href="https://docs.csc174.org/" target="_blank">About this course</a>
+		</div>
+	</section>
+
+
+
+	<section class="bottom-right">
+		<div class="team-reference">
+			<a href="secondary.php">View Our Teams</a>
+		</div>
+	</section>
+
+
+<!-- 	<section>
+	</section>
+
+	<section>
+	</section>
+
+	<section>
+	</section>
+
+	<section>
+	</section> -->
 </body>
 </html>
